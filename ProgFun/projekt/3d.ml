616e5fcfe29e238987c7557fc017b79057e2c446
foo <maciek.pacut@gmail.com>
2008-11-16 12:44:46 +0100

    3d: model to world

--- ProgFun/projekt/3d.ml
+++ ProgFun/projekt/3d.ml
@@ -1,23 +1,21 @@
 open Model;;
 
-let parse_model filename =
-    Parser.main Lexer.next (Lexing.from_channel (open_in filename))
-;;
+type world = vertex3d list;;
+
+let parse_model filename = Parser.main Lexer.next (Lexing.from_channel (open_in filename));;
 
 let print_model model =
   List.iter (fun (x,y,z) -> Printf.printf "vertex: %f %f %f\n" x y z) model.vertices;
   List.iter (fun (a,b,c) -> Printf.printf "face: %d %d %d\n" a b c) model.tris
 ;;
-(*
+
 let model_to_world model =
-  let v = vertices model
-  and f = faces model
-  in let get_vertex_3d index = let Model.Vertex (x,y,z) = v.(index-1) in (x,y,z)
-  in List.map (fun Model.Face (a,b,c)
-		 -> (get_vertex3d(a), get_vertex3d(b), get_vertex3d(c))) faces
+  let v = Array.of_list model.vertices
+  in List.map (fun (a,b,c)
+	      -> (v.(a-1), v.(b-1), v.(c-1))) model.tris
 ;;
 
-let print_vertex (x,y,z) = Printf.printf "[%f,%f,%f]" x y z;;
+let print_vertex (x,y,z) = Printf.printf "[%.2f,%.2f,%.2f]" x y z;;
 let print_tri3d (v1,v2,v3) =
   print_vertex v1;
   Printf.printf ", ";
@@ -28,14 +26,12 @@ let print_tri3d (v1,v2,v3) =
 ;;
   
 let print_world world = List.iter print_tri3d world;;
-*)
+
 let run () =
-  try
-    let filename = Sys.argv.(1)
-    in print_model (parse_model filename)
-  with
-    | Invalid_argument _ -> print_string ("Usage: "^(Sys.argv.(0))^" <model.obj>\n")
-    | Sys_error _ -> print_string ("File \""^(Sys.argv.(1))^"\" does not exist.\n")
+  let filename = Sys.argv.(1)
+  in let model = parse_model filename
+  in let world = model_to_world model
+  in print_world world
 ;;
   
 
