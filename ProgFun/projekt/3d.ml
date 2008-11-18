ea8d7582cd04cd00d80187c1cedd7cd12a4b44fc
foo <maciek.pacut@gmail.com>
2008-11-18 20:28:35 +0100

    3d: mała refaktoryzacja

--- ProgFun/projekt/3d.ml
+++ ProgFun/projekt/3d.ml
@@ -1,46 +1,46 @@
 open Model;;
 
-let parse_model filename = Parser.main Lexer.next (Lexing.from_channel (open_in filename));;
+let (screen_width, screen_height) = (600., 600.);;
+let projection_d = 1.;;
 
-let model_to_world model =
+let indices_to_coordinates model =
   let v = Array.of_list model.vertices
   in List.map (fun (a,b,c)
-	      -> (v.(a-1), v.(b-1), v.(c-1))) model.tris
-;;
+	      -> (v.(a-1), v.(b-1), v.(c-1))) model.tris;;
 
-let flatten_vertex d = function (x, y, z) -> (600. *. d *. x /. z, 600. *. d *. y /. z);;
-let flatten_tri d (a, b, c) = (flatten_vertex d a, flatten_vertex d b, flatten_vertex d c);;
+let parse_model filename =
+  let model = Parser.main Lexer.next (Lexing.from_channel (open_in filename))
+  in indices_to_coordinates model;;
+
+let flatten_vertex = function (x, y, z) -> (screen_width *. projection_d *. x /. z, screen_height *. projection_d *. y /. z);;
+let flatten_tri (a, b, c) = (flatten_vertex a, flatten_vertex b, flatten_vertex c);;
+let flatten_model model = List.map flatten_tri model;;
 
 let render_tri (v1,v2,v3) =
-  Graphics.draw_poly (Array.map (fun (x,y) -> (int_of_float x, int_of_float y)) [| v1; v2; v3 |])
-;;
-  
-let render_world world = List.iter (fun tri -> render_tri (flatten_tri 1. tri)) world;;
+  Graphics.draw_poly (Array.map (fun (x,y) ->
+				   (int_of_float x, int_of_float y)) [| v1; v2; v3 |]);;
+let render_model model = List.iter render_tri (flatten_model model);;
 
-
-let rec loop world =
+let rec loop model =
   Graphics.auto_synchronize false;
   Graphics.clear_graph ();
   
   Graphics.set_color (Graphics.rgb 0 0 0);
-  render_world world;
+  render_model model;
 
   Graphics.auto_synchronize true;
   let status = Graphics.wait_next_event [Graphics.Poll; Graphics.Key_pressed] in
-    if (not status.Graphics.keypressed) then loop world
+    if (not status.Graphics.keypressed) then loop model
 ;;
 
+let init_args = Printf.sprintf " %dx%d" (int_of_float screen_width) (int_of_float screen_height);;
+
 let init () =
-  Graphics.open_graph " 600x600";
+  Graphics.open_graph init_args;
   Graphics.set_window_title "3d rendering engine";
   let filename = Sys.argv.(1)
   in let model = parse_model filename
-  in let world = model_to_world model
-  in loop world
-;;
+  in loop model;;
 
 let () =
   init ();;
-
-
-
