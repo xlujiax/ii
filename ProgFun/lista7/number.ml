c104fa125bfa4eb57d1078a58151cb0fdfd4f836
foo <maciek.pacut@gmail.com>
2008-11-23 11:24:27 +0100

    FP: lepsze numerowanie bfs

--- ProgFun/lista7/number.ml
+++ ProgFun/lista7/number.ml
@@ -1,30 +1,16 @@
 type 'a btree = Leaf of 'a | Node of 'a btree * 'a * 'a btree;;
+type 'a bforest = 'a btree list;;
 
-let example = Node (Node (Leaf 'a', 'b', Leaf 'c'), 'd', Leaf 'e');;
+let ex = Node (Node (Leaf 'a', 'b', Leaf 'c'), 'd', Leaf 'e');;
+let ex2 = Node ( Node (Leaf 'c', 'b', Leaf 'd') , 'a' , Node (Leaf 'f', 'e' , Leaf 'g'));;
 
-let rec ref_tree = function
-  | Leaf _ -> Leaf (ref 0)
-  | Node (left, _, right) -> Node (ref_tree left, ref 0, ref_tree right)
+let rec bfnum' i = function
+  | [] -> []
+  | Leaf _ :: rest -> bfnum' (i + 1) rest @ [Leaf i]
+  | Node (left, _, right) :: rest ->
+      let right' :: left' :: rest' = bfnum' (i + 1) (rest @ [left] @ [right]) in
+	rest' @ [Node (left', i, right')];;
 
-let bfs tree =
-  let rec aux = function
-    | [] -> []
-    | ((Leaf _) as actual) :: rest -> actual :: aux (rest)
-    | (Node (left, _, right) as actual) :: rest -> actual :: aux (rest @ [left] @ [right])
-  in aux [tree];;
+let bfnum tree = List.hd (bfnum' 1 [tree]);;
 
-let number_lst lst =
-  let counter = ref 1 in
-  List.iter (function
-	       | Leaf slot ->         slot := !counter; incr counter
-	       | Node (_, slot, _) -> slot := !counter; incr counter) lst;;
-
-
-let rec map_tree f = function
-  | Leaf slot -> Leaf (f slot)
-  | Node (left, slot, right) -> Node (map_tree f left, f slot, map_tree f right);;
-
-let rec number_tree tree =
-  let rt = ref_tree tree in
-    number_lst (bfs rt);
-    map_tree (fun slot -> !slot) rt;;
+let res = Node (Node (Leaf 4, 2, Leaf 5), 1, Leaf 3);;
