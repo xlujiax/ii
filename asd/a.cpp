d9e5e7af821b660377c1cb627fd8959fe5dbd129
foo <foo@pc.(none)>
2010-03-20 12:13:09 +0100

    Zad A odp bez zliczania

--- asd/a.cpp
+++ asd/a.cpp
@@ -7,9 +7,73 @@
 
 using namespace std;
 
-// mapa z długości sznurków w ich liczność
-typedef map<int, int> smap;
-typedef smap::iterator siter;
+static const int MAX_DATA = 1000000 * 63;
+
+bool arr[MAX_DATA];
+int counter = 0;
+
+/*
+unsigned int count_ones()
+{
+  unsigned int count = 0;
+  for(int i = 0; i < MAX_DATA; ++i)
+    if(arr[i])
+      count ++;
+  return count;
+}
+
+void print()
+{
+  cout << "[ ";
+  for(int i = 0; i < MAX_DATA; ++i)
+    if(arr[i])
+      cout << i << " ";
+  cout << "]" << endl;
+}
+
+void binary(int n)
+{
+  while(n != 0)
+  {
+    if(n % 2 != 0)
+      cout << 1;
+    else
+      cout << 0;
+      
+    n = n / 2;
+  }
+  cout << endl;
+}*/
+
+void insert(int len, int amount)
+{
+  if(arr[len])
+  {
+    amount++;
+    arr[len] = false;
+    --counter;
+  }
+
+  if(amount == 1)
+  {
+    arr[len] = 1;
+    ++counter;
+  }
+  else
+  {
+    int inserter = len;
+    while(amount != 0)
+    {
+      if(amount % 2 != 0)
+      {
+	insert(inserter, 1);
+      }
+      
+      amount = amount / 2;
+      inserter *= 2;
+    }
+  }
+}
 
 int main()
 {
@@ -18,42 +82,17 @@ int main()
   int n;
   cin >> n;
 
-  smap str;
-  
   for(int i = 0; i < n; ++i)
   {
     int amount, len;
     cin >> len >> amount;
-    str[len] = amount;
+    insert(len, amount);
+
+    //cout << "insert " << len << " " << amount << " = ";
+    //binary(amount);
+    //print();
   }
 
-  int counter = 0;
-
-  while(!str.empty())
-  {
-    siter shortest = str.begin();
-    int amount = (*shortest).second;
-    int len = (*shortest).first;
-
-    str.erase(shortest);
-
-    if(amount == 1)
-    {
-      counter ++;
-    }
-    else
-    {
-      int insert = len;
-      while(amount != 0)
-      {
-	if(amount % 2 != 0)
-	  str[insert]++;
-	
-	amount = amount / 2;
-	insert *= 2;
-      }
-    }
-  }
   cout << counter << endl;
 
   return 0;
