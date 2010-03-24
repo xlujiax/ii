faead37215143509471d71e6a78c0f80e89484e4
foo <foo@pc.(none)>
2010-03-24 17:32:46 +0100

    C++: szablon funkcji testowej opakowany w strukturę

--- cpp/p02/z1.cpp
+++ cpp/p02/z1.cpp
@@ -33,142 +33,8 @@ template<typename Iter>
   }
 }
 
-template<typename Iter>
-  Iter next(Iter i)
-{
-  Iter tmp = i;
-  ++tmp;
-  return tmp;
-}
-
-template<typename Iter>
-  void quick_sort(Iter begin, Iter end)
-{
-  std::cout << "[ ";
-  std::copy(begin, end, std::ostream_iterator<int>(std::cout, " "));
-  std::cout << "]" << std::endl;
-    
-  if(begin == end)
-    return;
-  
-  if(next(begin) == end)
-    if(*begin > *end)
-    {
-      std::swap(*begin, *end);
-      return;
-    }
-  
-  const Iter orig_begin = begin;
-  const Iter orig_end = end;
-  
-  Iter pivot = begin;
-  begin++;
-  end--;
-
-  // [ pivot, begin, ..., end ]
-
-  while(begin != end && next(end) != begin)
-  {
-    while(*begin < *pivot) ++begin;
-    while(*end > *pivot) --end;
-
-    if(begin != end && next(end) != begin)
-    {
-      std::swap(*begin, *end);
-      ++begin;
-      --end;
-    }
-  }
-
-  std::swap(*pivot, *begin);
-  
-  std::cout << "[ ";
-  std::copy(orig_begin, orig_end, std::ostream_iterator<int>(std::cout, " "));
-  std::cout << "]" << std::endl;
-
-  quick_sort(orig_begin, begin);
-  quick_sort(begin, orig_end);
-}
-
-template<typename Iter>
-  void quick_sort2(Iter begin, Iter end)
-{
-  // W trakcie wykonywania algorytmu tracimy informację o oryginalnym przedziale,
-  // dlatego zapamiętujemy go w poniższych stałych:
-  const Iter first = begin;
-  const Iter last = end;
-
-  // przetwarzamy od elementu następnego po wybranym elemencie środkowym (first)
-  ++begin;
-  // przetwarzamy do końcowego elementu; end wskazuje na miejsce za ostatnim elementem, więc przesuwamy go na ostatni element
-  --end;
-
-  if(first != end && begin != end)
-  {
-    std::cout << "* middle is " << *first << std::endl;
-    std::copy(first, last, std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    while(begin != end && next(begin) != end)
-    {
-      std::cout << "[" << *begin << " " << *end << "]" << std::endl;
-      
-      while(*begin < *first && begin != end && next(begin) != end)
-      {
-	std::cout << "skip++ " << *begin << std::endl;
-	
-	++begin;
-      }
-      while(*end > *first && begin != end && next(begin) != end)
-      {
-	std::cout << "skip-- " << *end << std::endl;
-	
-	--end;
-      }
-
-      if(begin != end && next(begin) != end)
-      {
-	std::cout << "swap " << *begin << ", "<< *end << std::endl;
-  
-	std::swap(*begin, *end);
-	++begin;
-	--end;
-      }
-    }
-
-    if(next(begin) == end)
-    {
-      if(*begin < *first)
-	++begin;
-      else
-	++end;
-    }
-    
-    // { begin == end <- wskazują na środkowy element}
-    if(*begin <= *first)
-      std::swap(*begin, *first);
-    else
-    {
-      Iter t = begin;
-      t--;
-      std::swap(*t, *first);
-    }
-
-    std::cout << "po podziale " << std::endl;
-    std::copy(first, last, std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    quick_sort(first, begin);
-    quick_sort(begin, last);
-  }
-  else
-  {
-    if(*first > *end)
-    {
-      std::swap(*first, *end);
-    }
-  }
-}
+// generatory losowych wartości
+// {
 
 template <typename T>
   T random_value();
@@ -198,52 +64,113 @@ template <>
   return std::complex<float>(random_value<float>(), random_value<float>());
 }
 
-template<typename T, typename Iter>
-  bool test_aux(int N, Iter a_begin, Iter a_inserter, Iter b_begin, Iter b_inserter)
+// }
+// generatory losowych wartości
+
+template<typename T, typename Cont>
+  struct Test;
+
+template<typename T>
+  struct Test<T, T*>
 {
-  /*std::generate_n(a_inserter, N, random_value<T>);
-  std::copy(a_begin, a_inserter, b_inserter);
+  bool operator()(const int N) const
+  {
+    T* a = new T[N];
+    T* b = new T[N];
+    
+    std::generate_n(a, N, random_value<T>);
+    std::copy(a, a + N, b);
 
-  std::sort(a_begin, a_inserter);
-  bubble_sort(b_begin, b_inserter);
-
-  return equal(a_begin, a_inserter, b_begin);*/
-  return false;
-}
-
-template<typename T, typename Container>
-  bool test(int, Container);
+    std::sort(a, a + N);
+    bubble_sort(b, b + N);
+    
+    return std::equal(a, a + N, b);
+  }
+};
 
 template<
   typename T,
   template<
     typename U,
-    typename Alloc = std::allocator<U> >
-  class Container>
-  bool test(int N, Container<T> a)
+    typename Alloc = std::allocator<U>
+    > class Cont >
+  struct Test<T, Cont<T> >
 {
-  Container<T> b;
-  return test_aux<T, typename Container<T>::iterator>(N, a.begin(), std::back_inserter(a), b.begin(), std::back_inserter(b));
-}
+  bool operator()(const int N) const
+  {
+    Cont<T> a;
+    Cont<T> b;
+    
+    std::generate_n(std::back_inserter(a), N, random_value<T>);
+
+    b = a;
+
+    // ! back_inserter nie działa
+    // std::copy(a.begin, a.end(), std::back_inserter(b));
+
+    std::sort(a.begin(), a.end());
+    bubble_sort(b.begin(), b.end());
+    
+    return std::equal(a.begin(), a.end(), b.begin());
+  }
+};
+
+template<typename T>
+  struct Test<T, std::list<T> >
+{
+  bool operator()(const int N) const
+  {
+    std::list<T> a;
+    std::list<T> b;
+
+    std::generate_n(std::back_inserter(a), N, random_value<T>);
+
+    b = a;
+
+    // ! back_inserter nie działa
+    //std::copy(a.begin, a.end(), std::back_inserter(b));
+
+    a.sort();
+    bubble_sort(b.begin(), b.end());
+    
+    return std::equal(a.begin(), a.end(), b.begin());
+  }
+};
 
 template<>
-  bool test<char, std::string>(int N, std::string a)
+  struct Test<char, std::string >
 {
-  std::string b;
-  return test_aux<char, std::string::iterator>(N, a.begin(), std::back_inserter(a), b.begin(), std::back_inserter(b));
-}
-/*
-template<typename T>
-  bool test<T, T*>(int N, T* a)
-{
-  return false;
-  }*/
+  bool operator()(const int N) const
+  {
+    std::string a;
+    std::string b;
+
+    std::generate_n(std::back_inserter(a), N, random_value<char>);
+
+    b = a;
+
+    // ! back_inserter nie działa
+    //std::copy(a.begin, a.end(), std::back_inserter(b));
+
+    std::sort(a.begin(), a.end());
+    bubble_sort(b.begin(), b.end());
+    
+    return std::equal(a.begin(), a.end(), b.begin());
+  }
+};
 
 int main(int, char*[])
 {
   srand(time(0));
-  
-  if(test<std::string, std::vector>(10, std::vector<std::string>()))
+
+  bool test = 
+    Test<int, std::vector<int> >()(7) &&
+    Test<int, int* >()(7)             &&
+    Test<int, std::list<int> >()(7)   &&
+    Test<int, std::deque<int> >()(7)   &&
+    Test<char, std::string >()(7);
+
+  if(test)
     std::cout << "OK" << std::endl;
   else
     std::cout << "Niepowodzenie" << std::endl;
