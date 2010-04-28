db6409ac02364bcd03ba168b83276ed32b5560f6
foo <foo@pc.(none)>
2010-04-28 17:21:49 +0200

    +,-,/

--- cpp/p05/z1.cpp
+++ cpp/p05/z1.cpp
@@ -27,13 +27,46 @@ template<typename T>
 };
 
 template<typename T, typename L, typename R>
-  class binary_operator
+  class addition_operator
 {
   typename expr<L>::type l;
   typename expr<R>::type r;
 public:
-  binary_operator(const L& ll, const R& rr) : l(ll), r(rr)  {}
+  addition_operator(const L& ll, const R& rr) : l(ll), r(rr)  {}
   T operator[](int i) const { return l[i] + r[i]; }
+  int size() const { return (l.size() == 0) ? r.size() : l.size(); }
+};
+
+template<typename T, typename L, typename R>
+  class multiplication_operator
+{
+  typename expr<L>::type l;
+  typename expr<R>::type r;
+public:
+  multiplication_operator(const L& ll, const R& rr) : l(ll), r(rr)  {}
+  T operator[](int i) const { return l[i] * r[i]; }
+  int size() const { return (l.size() == 0) ? r.size() : l.size(); }
+};
+
+template<typename T, typename L, typename R>
+  class division_operator
+{
+  typename expr<L>::type l;
+  typename expr<R>::type r;
+public:
+  division_operator(const L& ll, const R& rr) : l(ll), r(rr)  {}
+  T operator[](int i) const { return l[i] / r[i]; }
+  int size() const { return (l.size() == 0) ? r.size() : l.size(); }
+};
+
+template<typename T, typename L, typename R>
+  class subtraction_operator
+{
+  typename expr<L>::type l;
+  typename expr<R>::type r;
+public:
+  subtraction_operator(const L& ll, const R& rr) : l(ll), r(rr)  {}
+  T operator[](int i) const { return l[i] - r[i]; }
   int size() const { return (l.size() == 0) ? r.size() : l.size(); }
 };
 
@@ -73,11 +106,35 @@ public:
 };
 
 template <typename T, int Sz, typename R1, typename R2>
-  vector<T, Sz, binary_operator<T, R1, R2> >
+  vector<T, Sz, addition_operator<T, R1, R2> >
   operator+(const vector<T, Sz, R1>& a, const vector<T, Sz, R2>& b)
 {
-  return vector<T, Sz, binary_operator<T,R1,R2> >(binary_operator<T,R1,R2>(a.get_rep(), b.get_rep()));
+  return vector<T, Sz, addition_operator<T,R1,R2> >(addition_operator<T,R1,R2>(a.get_rep(), b.get_rep()));
 }
+
+
+template <typename T, int Sz, typename R1, typename R2>
+  vector<T, Sz, multiplication_operator<T, R1, R2> >
+  operator*(const vector<T, Sz, R1>& a, const vector<T, Sz, R2>& b)
+{
+  return vector<T, Sz, multiplication_operator<T,R1,R2> >(multiplication_operator<T,R1,R2>(a.get_rep(), b.get_rep()));
+}
+
+template <typename T, int Sz, typename R1, typename R2>
+  vector<T, Sz, division_operator<T, R1, R2> >
+  operator/(const vector<T, Sz, R1>& a, const vector<T, Sz, R2>& b)
+{
+  return vector<T, Sz, division_operator<T,R1,R2> >(division_operator<T,R1,R2>(a.get_rep(), b.get_rep()));
+}
+
+
+template <typename T, int Sz, typename R1, typename R2>
+  vector<T, Sz, subtraction_operator<T, R1, R2> >
+  operator-(const vector<T, Sz, R1>& a, const vector<T, Sz, R2>& b)
+{
+  return vector<T, Sz, subtraction_operator<T,R1,R2> >(subtraction_operator<T,R1,R2>(a.get_rep(), b.get_rep()));
+}
+
 
 
 template<typename T>
@@ -87,6 +144,8 @@ template<typename T>
   static int initializations;
   static int additions;
   static int multiplications;
+  static int subtractions;
+  static int divisions;
   static int copies;
 public:
   numeric() {}
@@ -105,7 +164,7 @@ public:
     n.v = a.v + b.v;
     return n;
   }
-  friend numeric<T>& operator*(const numeric<T>& a, const numeric<T>& b)
+  friend numeric<T> operator*(const numeric<T>& a, const numeric<T>& b)
   {
     ++multiplications;
     
@@ -113,17 +172,47 @@ public:
     n.v = a.v * b.v;
     return n;
   }
+  friend numeric<T> operator/(const numeric<T>& a, const numeric<T>& b)
+  {
+    ++divisions;
+    
+    numeric n;
+    n.v = a.v / b.v;
+    return n;
+  }
+  friend numeric<T> operator-(const numeric<T>& a, const numeric<T>& b)
+  {
+    ++subtractions;
+    
+    numeric n;
+    n.v = a.v - b.v;
+    return n;
+  }
   operator T()
   {
     return v;
   }
 
+  static void reset_report()
+  {
+    initializations =
+      additions =
+      multiplications =
+      divisions =
+      subtractions =
+      copies = 0;
+  }
+
   static void usage_report()
   {
-    std::cout << "additions: " << additions << std::endl
+    std::cout << " *** " << std::endl
+	      << "additions: " << additions << std::endl
 	      << "multiplications: " << multiplications << std::endl
+	      << "divisions: " << divisions << std::endl
+	      << "subtractions: " << subtractions << std::endl
 	      << "copies: " << copies << std::endl
-	      << "initializations: " << initializations << std::endl;
+	      << "initializations: " << initializations << std::endl
+	      << " *** " << std::endl;
   }
 };
 
@@ -133,6 +222,10 @@ template<typename T>
   int numeric<T>::additions = 0;
 template<typename T>
   int numeric<T>::multiplications = 0;
+template<typename T>
+  int numeric<T>::subtractions = 0;
+template<typename T>
+  int numeric<T>::divisions = 0;
 template<typename T>
   int numeric<T>::copies = 0;
 
@@ -145,10 +238,15 @@ int main(int, char*[])
   u[0] = 1;
 
   vector<numeric<int>, 1> w;
-  w = v + u;
+  w[0] = 7;
+
+  vector<numeric<int>, 1> r;
+  r = v + u * w;
   
-  std::cout << w[0] << std::endl;
+  std::cout << r[0] << std::endl;
 
   numeric<int>::usage_report();
+  numeric<int>::reset_report();
+
   return 0;
 }
