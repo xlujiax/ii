69a014d927894c45960531266b27820f3e6dc11a
foo <foo@pc.(none)>
2010-04-28 18:21:41 +0200

    dot product - rozwijanie

--- cpp/p05/z1.cpp
+++ cpp/p05/z1.cpp
@@ -137,7 +137,7 @@ template<typename T, int Sz, int Get,
 	 typename Rep1, typename Rep2>
   struct dp
 {
-  static T value(const vector<T, Sz, Rep1>& a, const vector<T, Sz, Rep1>& b)
+  inline static T value(const vector<T, Sz, Rep1>& a, const vector<T, Sz, Rep1>& b)
   {
     return a[Get] * b[Get] + dp<T, Sz, Get + 1, Rep1, Rep2>::value(a, b);
   }
@@ -147,7 +147,7 @@ template<typename T, int Sz,
 	 typename Rep1, typename Rep2>
   struct dp<T, Sz, Sz, Rep1, Rep2>
 {
-  static T value(const vector<T, Sz, Rep1>&, const vector<T, Sz, Rep1>&)
+  inline static T value(const vector<T, Sz, Rep1>&, const vector<T, Sz, Rep1>&)
   {
     return 0;
   }
