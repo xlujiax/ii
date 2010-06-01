95a380fa3bf14b0b87178ac9203906b7d8ba45e7
maciek <maciek@pc.(none)>
2010-06-01 14:33:51 +0200

    Białe tło

--- lpp/game.c
+++ lpp/game.c
@@ -29,9 +29,9 @@ void draw()
 {
   for(int g = 0; g < num_graphs; ++g)
     graph_draw(graphs[g]);
-  
+
   // mouse
-  glColor3f(1,1,1);
+  glColor3f(0.0f, 0.0f, 0.0f);
   glBegin(GL_LINE_STRIP);
   for(float i = 0; i < 2 * 3.1415; i += 0.1)
     glVertex2f(mouse_size * cosf(i) + getMouseX(), mouse_size * sinf(i) + getMouseY());
--- lpp/init.c
+++ lpp/init.c
@@ -14,6 +14,7 @@ int getMouseY() { return mouse_y; }
 
 void loop()
 {
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
 
