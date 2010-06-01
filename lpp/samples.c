1cd2eab28ebf842796f7cf350d55a56cdd349371
maciek <maciek@pc.(none)>
2010-06-01 14:46:18 +0200

    Przykladowe wielomiany

---
+++ lpp/samples.c
@@ -0,0 +1,11 @@
+#include "samples.h"
+
+Bezier* sample_bezier_sinus()
+{
+  return 0;
+}
+
+Bezier* sample_bezier_cosinus()
+{
+  return 0;
+}
---
+++ lpp/samples.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "bezier.h"
+
+Bezier* sample_bezier_sinus();
+Bezier* sample_bezier_cosinus();
+
