88f7436bfce21830ac67fa945bfe37e986e8544e
maciek <maciek@pc.(none)>
2010-06-03 22:37:55 +0200

    Ppoprawienie bledu w Comeau

--- cpp/p08/z1.cpp
+++ cpp/p08/z1.cpp
@@ -7,14 +7,14 @@
 #include <map>
 using namespace std;
 
-void store(string s, map<string, int>& dict)
+void store(string s, map<string, int>* dict)
 {
   string::iterator new_end = remove_if(s.begin(),s.end(), not1(ptr_fun(::isalpha)));
   s.resize(new_end - s.begin());
   
   transform(s.begin(), s.end(), s.begin(), ::tolower);
   
-  dict[s]++;
+  (*dict)[s]++;
 }
 
 bool higher_word_count(pair<string, int> a, pair<string, int> b)
@@ -37,7 +37,7 @@ int main()
   map<string, int> dictionary;
   vector<pair<string, int> > word_count;
 
-  for_each(istream_iterator<string>(cin), istream_iterator<string>(), bind2nd(ptr_fun(store), dictionary));
+  for_each(istream_iterator<string>(cin), istream_iterator<string>(), bind2nd(ptr_fun(store), &dictionary));
   
   word_count.reserve(dictionary.size());
   copy(dictionary.begin(), dictionary.end(), back_inserter(word_count));
