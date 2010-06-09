739d423c1285da8f0f7550680fac05f879c0f975
Maciek Pacut <maciek.pacut@gmail.com>
2010-06-10 00:00:20 +0200

    p08

--- cpp/p08/z1.cpp
+++ cpp/p08/z1.cpp
@@ -7,14 +7,39 @@
 #include <map>
 using namespace std;
 
+struct word
+{
+  string& s;
+  string::iterator b;
+  word(string& S) : s(S), b(s.begin()) {}
+  string operator()()
+  {
+    string::iterator punc = find_if(find_if(b, s.end(), ptr_fun(::isalpha)), s.end(), not1(ptr_fun(::isalpha)));
+
+    string ret(b, punc);
+
+    b = punc;
+    
+    return ret;
+  }
+};
+
+void store_aux(string s, map<string, int>* dict)
+{
+  transform(s.begin(), s.end(), s.begin(), ::tolower);
+  string::iterator new_end = remove_if(s.begin(), s.end(), not1(ptr_fun(::isalpha)));
+  (*dict)[string(s.begin(), new_end)]++;
+}
+
 void store(string s, map<string, int>* dict)
 {
-  string::iterator new_end = remove_if(s.begin(),s.end(), not1(ptr_fun(::isalpha)));
-  s.resize(new_end - s.begin());
-  
-  transform(s.begin(), s.end(), s.begin(), ::tolower);
-  
-  (*dict)[s]++;
+  vector<string> vs;
+
+  int punc_left = count_if(s.begin(),s.end(), not1(ptr_fun(::isalpha)));
+  vs.reserve(punc_left);
+  generate_n(back_inserter(vs),punc_left + 1, word(s));
+
+  for_each(vs.begin(), vs.end(), bind2nd(ptr_fun(store_aux), dict));
 }
 
 bool higher_word_count(pair<string, int> a, pair<string, int> b)
