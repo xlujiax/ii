e526472190b1f3a9c0400ca7e763b9a4d2657678
Maciek Pacut <maciek.pacut@gmail.com>
2010-06-13 17:18:07 +0200

    C++: testy

--- cpp/p09/z1.cpp
+++ cpp/p09/z1.cpp
@@ -20,7 +20,7 @@ public:
   }
   void pop()
   {
-    std::pop_heap(seq.begin(), seq.end());
+    std::pop_heap(seq.begin(), seq.end(), Cmp());
     seq.pop_back();
   }
   T& front()
@@ -35,20 +35,49 @@ public:
   {
     return seq.empty();
   }
+  void pr()
+  {
+    for(typename Sequence<T>::iterator i = seq.begin(); i != seq.end(); ++i)
+      std::cout << *i << '\n';
+  }
 };
 
 int main(int, char*[])
 {
-  priority_queue<int> pq;
-  pq.push(7);
-  pq.push(3);
-  pq.push(2);
-  pq.push(10);
-  pq.push(1);
-  while(!pq.empty())
   {
-    std::cout << pq.front() << '\n';
-    pq.pop();
+    std::cout << "Kolejka liczb\n";
+    priority_queue<int> pq;
+    pq.push(7);
+    pq.push(3);
+    pq.push(2);
+    pq.push(10);
+    pq.push(1);
+    
+    while(!pq.empty())
+    {
+      std::cout << pq.front() << ' ';
+      pq.pop();
+    }
+    std::cout << std::endl;
   }
+
+  {
+    std::cout << "Kolejka liczb posortowana w odwrotnej kolejnosci\n";
+    priority_queue<int, std::deque, std::greater<int> > pq;
+    pq.push(7);
+    pq.push(3);
+    pq.push(2);
+    pq.push(10);
+    pq.push(1);
+    
+    while(!pq.empty())
+    {
+      std::cout << pq.front() << ' ';
+      pq.pop();
+    }
+    std::cout << std::endl;
+  }
+
+  
   return 0;
 }
