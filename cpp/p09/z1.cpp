3b60a0c994c971910ad1098fbf6fb9504d277301
Maciek Pacut <maciek.pacut@gmail.com>
2010-06-13 17:08:13 +0200

    priority queue

--- cpp/p09/z1.cpp
+++ cpp/p09/z1.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <deque>
+#include <algorithm>
+
+template<typename T,
+	 template <
+	   typename Element,
+	   typename = std::allocator<Element>
+	   > class Sequence = std::deque,
+	 typename Cmp = std::less<T> >
+  class priority_queue
+{
+private:
+  Sequence<T> seq;
+public:
+  void push(const T& t)
+  {
+    seq.push_back(t);
+    std::push_heap(seq.begin(), seq.end(), Cmp());
+  }
+  void pop()
+  {
+    std::pop_heap(seq.begin(), seq.end());
+    seq.pop_back();
+  }
+  T& front()
+  {
+    return *seq.begin();
+  }
+  const T& front() const
+  {
+    return *seq.begin();
+  }
+  bool empty() const
+  {
+    return seq.empty();
+  }
+};
 
 int main(int, char*[])
 {
+  priority_queue<int> pq;
+  pq.push(7);
+  pq.push(3);
+  pq.push(2);
+  pq.push(10);
+  pq.push(1);
+  while(!pq.empty())
+  {
+    std::cout << pq.front() << '\n';
+    pq.pop();
+  }
   return 0;
 }
