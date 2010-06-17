cf94cff6a1ac3af832e9fbc4b2dee0b95fd62ae3
maciek <maciek@maciek-laptop.(none)>
2010-06-17 10:18:37 +0200

    Wyrzucenie lpp, przeniesienie go do innego repozytorium

--- lpp/init.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "game.h"
-#include <GL/glut.h>
-#include <math.h>
-
-int getMouseX();
-int getMouseY();
--- lpp/reduction.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "curve.h"
-
-Curve* curve_degree_reduction(Curve* c, int reduced_degree);
-Curve* curve_degree_reduction_rec(Curve* c, int reduced_degree);
