914eaffb1f01f8d9679779c8c30b331f57c4668f
maciek <maciek@pc.(none)>
2011-03-02 13:56:47 +0100

    addition

--- oop_eng/e3/bigint.cpp
+++ oop_eng/e3/bigint.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <vector>
 #include <iterator>
+#include <cassert>
 
 struct digit
 {
@@ -45,10 +46,119 @@ void printbigint(const digit* big)
   }
 }
 
+void deletebigint(digit* big)
+{
+  while(big)
+  {
+    digit* temp = big;
+    big = big->next;
+    delete temp;
+  }
+}
+
+char i2d(int i)
+{
+  return i + '0';
+}
+
+int d2i(char d)
+{
+  return d - '0';
+}
+
+// moves foreward carry
+void fix_over_9(digit* big)
+{
+  while(big)
+  {
+    if(d2i(big->n) > 9)
+    {
+      int carry = d2i(big->n) / 10;
+      big->n = i2d(d2i(big->n) % 10);
+
+      assert(carry > 0);
+
+      if(big->next)
+	big->next->n = i2d(d2i(big->next->n) + carry);
+      else
+      {
+	big->next = new digit;
+	big->next->next = 0;
+	big->next->n = i2d(carry);
+      }
+    }
+      
+    big = big->next;
+  }
+}
+
+digit* copy(const digit* lst)
+{
+  if(!lst) return 0;
+
+  // list not empty; creating new list and copying first element of original list into it
+  digit* new_lst = new digit;
+  new_lst->n = lst->n;
+
+  // iteration from second element of original list
+  lst = lst->next;
+
+  // iteration creates new cells and attaches them to end of new list
+  digit* last_cell = new_lst;
+  while(lst)
+  {
+    digit* new_cell = new digit;
+    new_cell->n = lst->n;
+    last_cell->next = new_cell;
+    last_cell = new_cell; // actualize last cell as we attached new cell in line one above
+    lst = lst->next;
+  }
+  
+  last_cell->next = 0;
+  return new_lst;
+}
+
+void append(digit* begin, digit* end)
+{
+  assert(begin);
+  
+  while(begin->next)
+  {
+    begin = begin->next;
+  }
+  begin->next = end;
+}
+
+void addto(const digit* from, digit* to)
+{
+  digit* original_to = to;
+  while(from && to)
+  {
+    to->n = i2d(d2i(to->n) + d2i(from->n));
+    to = to->next;
+    from = from->next;
+  }
+
+  // from is longer then to
+  if(from)
+  {
+    // copy rest of 'from' to 'to'
+    digit* rest_of_from = copy(from);
+    append(original_to, rest_of_from);
+  }
+
+  fix_over_9(original_to);
+}
 
 int main(int argc, char* argv[])
 {
-  digit* d = buildbigint(182773087);
-  printbigint(d);
+  digit* a = buildbigint(119);
+  digit* b = buildbigint(3333333);
+  printbigint(a);
+  printbigint(b);
+  addto(a, b);
+  printbigint(a);
+  printbigint(b);
+
   return 0;
 }
