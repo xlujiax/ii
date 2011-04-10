99a17fe3ed4de17ecdcd22bcc541ba35d91d40ac
maciek <maciek@pc.(none)>
2011-04-10 18:11:18 +0200

    Redukcja zaleznosci

--- sieci/p1/traceroute.c
+++ sieci/p1/traceroute.c
@@ -1,15 +1,4 @@
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <netinet/udp.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <errno.h>
-#include <assert.h>
-#include <unistd.h>
 #include "sockwrap.h"
 #include "send_echo_request.h"
 
