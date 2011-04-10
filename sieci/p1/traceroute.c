7be16cc05a84ffd2e1d9c3d8ac0c357d6b2df75b
maciek <maciek@pc.(none)>
2011-04-10 17:17:49 +0200

    argument linii polecen

--- sieci/p1/traceroute.c
+++ sieci/p1/traceroute.c
@@ -12,8 +12,14 @@
 #include "sockwrap.h"
 #include "icmp.h"
 
-
-int main()
+int main(int argc, char* argv[])
 {
+  if(argc != 2)
+  {
+    printf("Usage: traceroute ip_address\n");
+    return -1;
+  }
+
+  printf("ip: %s\n", argv[1]);
   return 0;
 }
