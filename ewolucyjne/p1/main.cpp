8977285ee3728c8b78296437a8006badabc21e68
maciek <maciek@debian>
2011-10-06 19:15:27 +0200

    poprawny wektor wygenerowany ewolucyjnie

--- ewolucyjne/p1/main.cpp
+++ ewolucyjne/p1/main.cpp
@@ -6,77 +6,124 @@
 #include <algorithm>
 using namespace std;
 
+void print_result();
+
 int d = 80;
 double theta = 1.0 / 30;
+
+typedef vector<bool> individual;
 
 
 double uniform_random(double a, double b)
 {
-    return a + (b - a) * ((double)rand() / (double)RAND_MAX);
+  double ret = a + (b - a) * ((double)rand() / (double)RAND_MAX);
+  assert(a <= ret);
+  assert(ret <= b);
+  return ret;
 }
 
 bool binary_random(double p)
 {
-    if(uniform_random(0,1) < p)
-        return 1;
-    return 0;
+  assert(0.0 <= p);
+  assert(p <= 1.0);
+  if(uniform_random(0.0, 1.0) < p)
+    return 1;
+  return 0;
 }
 
 
-vector<double> p;
+vector<double> probabilities;
+
 void init_prob_vector()
 {
-    p.reserve(d);
-    fill_n(back_inserter(p), d, 0.5);
+  probabilities.reserve(d);
+  fill_n(back_inserter(probabilities), d, 0.5);
 }
 
-vector<bool> rand_individual()
+individual rand_individual()
 {
-    vector<bool> v;
-    v.reserve(d);
-    for(int i = 0; i < d; ++i)
-        v[i] = binary_random(p[i]);
-    return v;
+  individual v;
+  v.resize(d);
+  for(int i = 0; i < d; ++i)
+    v[i] = binary_random(probabilities[i]);
+  return v;
 }
 
-
-void cga(double (*F)(vector<bool>))
+void print_individual(const individual& i)
 {
-    init_prob_vector();
+  cout << "[ ";
+  for(int j = 0; j < i.size(); ++j)
+    cout << i[j] << ' ';
+  cout << "]\n";
+}
 
-    vector<bool> x1 = rand_individual();
-    vector<bool> x2 = rand_individual();
+void cga(double (*F)(const individual&), bool (*terminate)(const individual&, const individual&))
+{
+  init_prob_vector();
 
-    F(x1);
-    F(x2);
+  individual x1 = rand_individual();
+  individual x2 = rand_individual();
 
-    while(!terminate)
+  F(x1);
+  F(x2);
+
+  while(!terminate(x1, x2))
+  {
+    individual x_plus;
+    individual x_minus;
+
+    if(F(x1) >= F(x2))
     {
-        for(int k = 0; k < d; ++k)
-        {
-            if(x1[k] && !x2[k])
-	    {
-	      
-	    }
-        }
+      x_plus = x1;
+      x_minus = x2;
+    }
+    else
+    {
+      x_plus = x2;
+      x_minus = x1;
     }
 
-    cout << "p:\n";
-    for(int i = 0; i < d; ++i)
-        cout << p[i] << ' ';
-    cout << endl;
+    for(int k = 0; k < d; ++k)
+    {
+      if(x_plus[k] && !x_minus[k])
+	probabilities[k] += theta;
+      if(!x_plus[k] && x_minus[k])
+	probabilities[k] -= theta;
+    }
+
+    x1 = rand_individual();
+    x2 = rand_individual();
+  }
+
+  print_result();
 }
 
-double onemax(vector<bool> v)
+void print_result()
 {
-    return (double)count(v.begin(), v.end(), true);
+  cout << "resulting vector of probabilities:\n";
+  for(int i = 0; i < d; ++i)
+    cout << probabilities[i] << ' ';
+  cout << endl;
+}
+
+double onemax(const individual& v)
+{
+  return (double)count(v.begin(), v.end(), true);
+}
+
+bool terminate(const individual&, const individual&)
+{
+  static int counter = 0;
+  if(counter++ >= 100000)
+    return true;
+  return false;
 }
 
 int main()
 {
-    srand(time(0));
+  srand(time(0));
 
-    cga(onemax);
+  cga(onemax, terminate);
 
-    return 0;
+  return 0;
 }
