5fdcbedadcee2b939dcdc15a30a6456ac0e0917a
maciek <maciek@debian>
2011-10-09 17:44:40 +0200

    lambda usage

--- pgk/p1/main.cpp
+++ pgk/p1/main.cpp
@@ -8,43 +8,20 @@
 #include <iostream>
 using namespace std;
 
+// poznizsze obiekty musza byc globalne; jesli sa w funkcji main to wystepuje blad:
+// error: cannot convert ‘main(int, char**)::<lambda(char)>’ to ‘void (*)(char)’ in assignment
 game pong;
 timer frame_timer;
-
-void setup_opengl(const int width, const int height)
-{
-  glViewport(0, 0, width, height);
-  gluOrtho2D(0, width, height, 0);
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-}
-
-void keyup(const char k)
-{
-  pong.keyup(k);
-}
-
-void keydown(const char k)
-{
-  pong.keydown(k);
-}
-
-void frame()
-{
-  const float delta_time = frame_timer.delta_time();
-  pong.animate(delta_time);
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  pong.draw();
-
-  SDL_GL_SwapBuffers();
-}
 
 int main(int argc, char* argv[])
 {
   window wnd;
 
-  wnd.setup_opengl = setup_opengl;
+  wnd.setup_opengl = [](const int width, const int height) {
+    glViewport(0, 0, width, height);
+    gluOrtho2D(0, width, height, 0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+  };
 
   if(!wnd.setup(640, 480))
     return 1;
@@ -52,11 +29,19 @@ int main(int argc, char* argv[])
   pong.init();
   frame_timer.init();
 
-  // why not: wnd.keydown = [&](const key k) { pong.keydown(k); } ?
-  wnd.keydown = keydown;
-  wnd.keyup = keyup;
+  wnd.keydown = [&](const char k) { pong.keydown(k); };
+  wnd.keyup = [&](const char k) { pong.keyup(k); };
 
-  wnd.frame = frame;
+  wnd.frame = [&]() {
+    const float delta_time = frame_timer.delta_time();
+    pong.animate(delta_time);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    pong.draw();
+
+    SDL_GL_SwapBuffers();
+  };
 
   wnd.main_loop();
 
