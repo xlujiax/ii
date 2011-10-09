1a0162c85883f5a48bb069c47b4d858addb38b58
maciek <maciek@debian>
2011-10-09 17:37:11 +0200

    fixed empty setup function passed error

--- pgk/p1/main.cpp
+++ pgk/p1/main.cpp
@@ -5,14 +5,17 @@
 #include "timer.hpp"
 #include "game.hpp"
 
+#include <iostream>
+using namespace std;
+
 game pong;
 timer frame_timer;
 
 void setup_opengl(const int width, const int height)
 {
-   glViewport(0, 0, width, height);
-   gluOrtho2D(0, width, height, 0);
-   glClearColor(0.0, 0.0, 0.0, 0.0);
+  glViewport(0, 0, width, height);
+  gluOrtho2D(0, width, height, 0);
+  glClearColor(0.0, 0.0, 0.0, 0.0);
 }
 
 void keyup(const char k)
@@ -27,19 +30,21 @@ void keydown(const char k)
 
 void frame()
 {
-    const float delta_time = frame_timer.delta_time();
-    pong.animate(delta_time);
+  const float delta_time = frame_timer.delta_time();
+  pong.animate(delta_time);
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    pong.draw();
+  pong.draw();
 
-    SDL_GL_SwapBuffers();
+  SDL_GL_SwapBuffers();
 }
 
 int main(int argc, char* argv[])
 {
   window wnd;
+
+  wnd.setup_opengl = setup_opengl;
 
   if(!wnd.setup(640, 480))
     return 1;
@@ -47,12 +52,10 @@ int main(int argc, char* argv[])
   pong.init();
   frame_timer.init();
 
-  wnd.setup_opengl = setup_opengl;
-
   // why not: wnd.keydown = [&](const key k) { pong.keydown(k); } ?
   wnd.keydown = keydown;
   wnd.keyup = keyup;
-  
+
   wnd.frame = frame;
 
   wnd.main_loop();
