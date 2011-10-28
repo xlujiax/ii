80b4f9ba323dca2878e99293713c81be464ce967
rafal <rafal@rafalPC.(none)>
2011-10-28 13:13:50 +0200

    Drawing simple cube

--- pgk/p2/main.cpp
+++ pgk/p2/main.cpp
@@ -8,14 +8,44 @@
 timer frame_timer;
 viewer main_viewer;
 
+GLfloat light_diffuse[] = {1.0, 0.0, 0.0, 1.0};  /* Red diffuse light. */
+GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};  /* Infinite light location. */
+GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
+  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
+  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
+GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
+  {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
+  {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
+GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
+
 int main(int argc, char* argv[])
 {
   window wnd;
 
   wnd.setup_opengl = [](const int width, const int height) {
-    glViewport(0, 0, width, height);
-    gluOrtho2D(0, width, height, 0);
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    /* Enable a single OpenGL light. */
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+
+    /* Use depth buffering for hidden surface elimination. */
+    glEnable(GL_DEPTH_TEST);glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(40.0, // field of view in degrees
+      1.0,               // aspect ratio
+      1.0,               // z near
+      10.0               // z far
+		   );
+    glMatrixMode(GL_MODELVIEW);
+    gluLookAt(0.0, 0.0, 5.0,  // eye
+      0.0, 0.0, 0.0,          // at
+      0.0, 1.0, 0.0           // up is in positive Y direction
+	      );
+    /* Adjust cube position to be asthetic angle. */
+    glTranslatef(0.0, 0.0, -1.0);
+    glRotatef(60, 1.0, 0.0, 0.0);
+    glRotatef(-20, 0.0, 0.0, 1.0);
   };
 
   if(!wnd.setup(640, 480))
@@ -23,6 +53,14 @@ int main(int argc, char* argv[])
 
   frame_timer.init();
   main_viewer.init();
+  /* Setup cube vertex data. */
+  v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
+  v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
+  v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
+  v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
+  v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
+  v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
+
 
   wnd.keydown = [&](const char k) {  };
   wnd.keyup = [&](const char k) {  };
@@ -33,6 +71,16 @@ int main(int argc, char* argv[])
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     main_viewer.draw();
+
+    for (int i = 0; i < 6; i++) {
+      glBegin(GL_QUADS);
+      glNormal3fv(&n[i][0]);
+      glVertex3fv(&v[faces[i][0]][0]);
+      glVertex3fv(&v[faces[i][1]][0]);
+      glVertex3fv(&v[faces[i][2]][0]);
+      glVertex3fv(&v[faces[i][3]][0]);
+      glEnd();
+    }
     SDL_GL_SwapBuffers();
   };
 
