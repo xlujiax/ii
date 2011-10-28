0bdd5e5ab4ce60e625f6b3e0af07615d4d256f66
Maciek <maciek.pacut@gmail.com>
2011-10-28 18:06:15 +0200

    No segv, but no image al well

--- pgk/p2/obj_format.cpp
+++ pgk/p2/obj_format.cpp
@@ -1,34 +1,87 @@
 #include "obj_format.hpp"
 
-GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
-  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
-GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
-  {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
-  {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
-GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
-
-GLuint vboId;                              // ID of VBO
+GLuint vboId1;
+GLuint vboId2;
+GLuint vCount;
+GLfloat* vertices;
+GLuint* indices;
 
 void obj_format::read_from_file(const char* filename)
 {
-  /* Setup cube vertex data. */
-  v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
-  v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
-  v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
-  v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
-  v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
-  v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
+  vCount = 8;
+  vertices = new GLfloat[vCount*3];
+  vertices[0] = -1;
+  vertices[1] = -1;
+  vertices[2] = -1;
+  
+  vertices[3] =  1;
+  vertices[4] = -1;
+  vertices[5] = -1;
+  
+  vertices[6] =  1;
+  vertices[7] =  1;
+  vertices[8] = -1;
+  
+  vertices[9] = -1;
+  vertices[10] =  1;
+  vertices[11] = -1;
+  
+  vertices[12] = -1;
+  vertices[13] = -1;
+  vertices[14] =  1;
+  
+  vertices[15] =  1;
+  vertices[16] = -1;
+  vertices[17] =  1;
+  
+  vertices[18] =  1;
+  vertices[19] =  1;
+  vertices[20] =  1;
+  
+  vertices[21] = -1;
+  vertices[22] =  1;
+  vertices[23] =  1;
 
-  // generate a new VBO and get the associated ID
-  glGenBuffersARB(1, &vboId);
+  indices = new GLuint[6*4];
+  indices[0]  = 0;
+  indices[1]  = 1;
+  indices[2]  = 2;
+  indices[3]  = 3;
 
-  // bind VBO in order to use
-  glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId);
+  indices[4]  = 4;
+  indices[5]  = 5;
+  indices[6]  = 6;
+  indices[7]  = 7;
 
-  const int dataSize = 8 * 3 * sizeof(float);
-  // upload data to VBO
-  glBufferDataARB(GL_ARRAY_BUFFER_ARB, dataSize, v, GL_STATIC_DRAW_ARB);
+  indices[8]  = 0;
+  indices[9]  = 1;
+  indices[10] = 5;
+  indices[11] = 4;
+
+  indices[12] = 0;
+  indices[13] = 3;
+  indices[14] = 7;
+  indices[15] = 4;
+
+  indices[16] = 3;
+  indices[17] = 2;
+  indices[18] = 6;
+  indices[19] = 7;
+
+  indices[20] = 1;
+  indices[21] = 2;
+  indices[22] = 6;
+  indices[23] = 5;
+
+  glGenBuffersARB(1, &vboId1);
+  glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId1);
+  glBufferDataARB(GL_ARRAY_BUFFER_ARB, 8 * 3 * sizeof(float), vertices, GL_STATIC_DRAW_ARB);
+  delete []vertices;
+
+  glGenBuffersARB(1, &vboId1);
+  glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, vboId1);
+  glBufferDataARB(GL_ELEMENT_ARRAY_BUFFER_ARB, 6 * 4 * sizeof(GLuint), indices, GL_STATIC_DRAW_ARB);
+  delete []indices;
 }
 
 void obj_format::draw() const
@@ -36,12 +89,12 @@ void obj_format::draw() const
   printf("step0\n");
 
   // bind VBOs for vertex array and index array
-  glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId);         // for vertex coordinates
-  //glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, vboId2); // for indices
+  glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId1);         // for vertex coordinates
+  glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, vboId2); // for indices
 
   printf("step2\n");
   // do same as vertex array except pointer
-  glEnableClientState(GL_VERTEX_ARRAY);             // activate vertex coords array
+  glEnableClientState(GL_INDEX_ARRAY);             // activate vertex coords array
   glVertexPointer(3, GL_FLOAT, 0, 0);               // last param is offset, not ptr
 
   printf("step3\n");
@@ -56,16 +109,4 @@ void obj_format::draw() const
   glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
   glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, 0);
   printf("step6\n");
-
-/*
-  for (int i = 0; i < 6; i++) {
-    glBegin(GL_QUADS);
-    glNormal3fv(&n[i][0]);
-    glVertex3fv(&v[faces[i][0]][0]);
-    glVertex3fv(&v[faces[i][1]][0]);
-    glVertex3fv(&v[faces[i][2]][0]);
-    glVertex3fv(&v[faces[i][3]][0]);
-    glEnd();
-  }
-*/
 }
