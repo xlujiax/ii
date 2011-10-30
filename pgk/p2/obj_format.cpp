3de9262f5e7e3b9103fa69fe69affe9bf0659f17
Maciek <maciek.pacut@gmail.com>
2011-10-30 20:18:00 +0100

    Appliaction of line classification

--- pgk/p2/obj_format.cpp
+++ pgk/p2/obj_format.cpp
@@ -36,7 +36,7 @@ std::vector<std::string> obj_format::file_to_memory(
   return lines;
 }
 
-line_type classify_line(const std::string& line) const
+obj_format::line_type obj_format::classify_line(const std::string& line) const
 {
   if(line.substr(0,2) == "v ") return line_type::vertex;
   else if(line.substr(0,3) == "vn ") return line_type::normal;
@@ -55,53 +55,63 @@ void obj_format::read_from_file(const char* filename)
   auto model = file_to_memory(filename);
   for(auto line : model)
   {
-    if(line[0] == 'v' && line[1] == ' ')
+    switch(classify_line(line))
     {
-      float x,y,z;
-      sscanf(line.c_str(), "v %f %f %f", &x, &y, &z);
-      vs.push_back({ x, y, z });
-    }
-    else if(line[0] == 'v' && line[1] == 'n' && line[2] == ' ')
-    {
-      float x,y,z;
-      sscanf(line.c_str(), "vn %f %f %f", &x, &y, &z);
-      ns.push_back({ x, y, z });
-    }
-    else if(line[0] == 'f' && line[1] == ' ')
-    {
-      int v[4];
-      int n[4];
-
-      sscanf(line.c_str(), "f %d//%d %d//%d %d//%d %d//%d",
-	&v[0], &n[0],
-	&v[1], &n[1],
-	&v[2], &n[2],
-	&v[3], &n[3]
-	     );
-      for(int i = 0; i < 4; ++i)
-      {
-	vertex vx = {
-	  vs[v[i] - 1].x, vs[v[i] - 1].y, vs[v[i] - 1].z,
-	  ns[n[i] - 1].x, ns[n[i] - 1].y, ns[n[i] - 1].z,
-	  0, 0
-	};
-
-	const int index_in_vbo = v[i] - 1; // could be index of normal or texture
-	if(map_nvs.end() == map_nvs.find(index_in_vbo))
-	  map_nvs[index_in_vbo] = vx;
-	else
+      case line_type::comment:
+      case line_type::unclassified:
+	break;
+	
+      case line_type::vertex:
 	{
-	  vertex before = (*map_nvs.find(index_in_vbo)).second;
-	  assert(before.x == vx.x);
-	  assert(before.y == vx.y);
-	  assert(before.z == vx.z);
-	  assert(before.nx == vx.nx);
-	  assert(before.ny == vx.ny);
-	  assert(before.nz == vx.nz);
+	  float x,y,z;
+	  sscanf(line.c_str(), "v %f %f %f", &x, &y, &z);
+	  vs.push_back({ x, y, z });
 	}
+	break;
+      case line_type::normal:
+	{
+	  float x,y,z;
+	  sscanf(line.c_str(), "vn %f %f %f", &x, &y, &z);
+	  ns.push_back({ x, y, z });
+	}
+	break;
+      case line_type::face:
+	{
+	  int v[4];
+	  int n[4];
+
+	  sscanf(line.c_str(), "f %d//%d %d//%d %d//%d %d//%d",
+	    &v[0], &n[0],
+	    &v[1], &n[1],
+	    &v[2], &n[2],
+	    &v[3], &n[3]
+		 );
+	  for(int i = 0; i < 4; ++i)
+	  {
+	    vertex vx = {
+	      vs[v[i] - 1].x, vs[v[i] - 1].y, vs[v[i] - 1].z,
+	      ns[n[i] - 1].x, ns[n[i] - 1].y, ns[n[i] - 1].z,
+	      0, 0
+	    };
+
+	    const int index_in_vbo = v[i] - 1; // could be index of normal or texture
+	    if(map_nvs.end() == map_nvs.find(index_in_vbo))
+	      map_nvs[index_in_vbo] = vx;
+	    else
+	    {
+	      vertex before = (*map_nvs.find(index_in_vbo)).second;
+	      assert(before.x == vx.x);
+	      assert(before.y == vx.y);
+	      assert(before.z == vx.z);
+	      assert(before.nx == vx.nx);
+	      assert(before.ny == vx.ny);
+	      assert(before.nz == vx.nz);
+	    }
        
-	fs.push_back(index_in_vbo);
-      }
+	    fs.push_back(index_in_vbo);
+	  }
+	}
+	break;
     }
   }
 
