314bcaaf0d4e7f0edb41f57d29c80d7d10e45aa3
Maciek <maciek.pacut@gmail.com>
2011-10-31 22:57:20 +0100

    Displaying textured model

--- pgk/p2/obj_format.cpp
+++ pgk/p2/obj_format.cpp
@@ -169,6 +169,10 @@ void obj_format::draw() const
   glEnableClientState(GL_NORMAL_ARRAY);
   glNormalPointer(GL_FLOAT, sizeof(vertex), BUFFER_OFFSET(3 * sizeof(float)));
 
+  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+  glClientActiveTexture(GL_TEXTURE0);
+  glTexCoordPointer(2, GL_FLOAT, sizeof(vertex), BUFFER_OFFSET(5 * sizeof(float)));
+  
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_vbo);
   glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 
