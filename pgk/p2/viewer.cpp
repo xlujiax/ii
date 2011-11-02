19b9f8e8137ee141c3117322e6e7aa24f4489552
Maciek <maciek.pacut@gmail.com>
2011-11-02 19:16:26 +0100

    Almost arcball

--- pgk/p2/viewer.cpp
+++ pgk/p2/viewer.cpp
@@ -11,6 +11,8 @@ void viewer::init()
   r3 = 11.0;
 
   arc_ball_radius = 3.0;
+  
+  glTranslatef(0.0, 0.0, zoom);
 }
 
 void viewer::animate(float)
@@ -19,14 +21,7 @@ void viewer::animate(float)
 
 void viewer::draw() const
 {
-  glPushMatrix();
-  glTranslatef(0.0, 0.0, zoom);
-  glRotatef(r1, 1.0, 0.0, 0.0);
-  glRotatef(r2, 0.0, 1.0, 0.0);
-  glRotatef(r3, 0.0, 0.0, 1.0);
-
   model.draw();
-  glPopMatrix();
 }
 
 void viewer::mousewheelup()
@@ -57,7 +52,7 @@ vec3 viewer::screen_to_arc(const vec2& s) const
   assert(-1.0 <= s.y);
   assert(s.y <= 1.0);
   
-  return { s.x, s.y,
+  return { -s.x, s.y,
       sqrtf(arc_ball_radius*arc_ball_radius - s.length()) };
 }
 
@@ -72,9 +67,11 @@ void viewer::mousemotion(const float x, const float y)
     const vec3 axis = vec3::cross(actual_arc, last_arc);
     const float angle = vec3::angle(actual_arc, last_arc);
 
-    glPushMatrix();
-    glRotatef(angle, axis.x, axis.y, axis.z);
-    glPopMatrix();
+    //float current_matrix[16];
+    //glGetFloatv(GL_MODELVIEW_MATRIX, current_matrix);
+    //glLoadIdentity();
+    glRotatef(rad_to_deg(angle), axis.x, axis.y, axis.z);
+    //glMultMatrixf(current_matrix);
     
     // use last_click
     last_click = { x, y };
