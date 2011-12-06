2c561e38ce50c50a38799d8058c9960fe3e9c1f1
Maciek <maciek.pacut@gmail.com>
2011-12-06 11:57:17 +0100

    Attempt to randomly generate map

--- pgk/p3/loader.hpp
+++ pgk/p3/loader.hpp
@@ -1,64 +1,41 @@
 #pragma once
 
-const float vertexData2[] = {
-  0.25f,  0.25f, 0.75f, 1.0f,
-  0.25f, -0.25f, 0.75f, 1.0f,
-  -0.25f,  0.25f, 0.75f, 1.0f,
-
-  0.25f, -0.25f, 0.75f, 1.0f,
-  -0.25f, -0.25f, 0.75f, 1.0f,
-  -0.25f,  0.25f, 0.75f, 1.0f,
-
-  0.25f,  0.25f, -0.75f, 1.0f,
-  -0.25f,  0.25f, -0.75f, 1.0f,
-  0.25f, -0.25f, -0.75f, 1.0f,
-
-  0.25f, -0.25f, -0.75f, 1.0f,
-  -0.25f,  0.25f, -0.75f, 1.0f,
-  -0.25f, -0.25f, -0.75f, 1.0f,
-
-  -0.25f,  0.25f,  0.75f, 1.0f,
-  -0.25f, -0.25f,  0.75f, 1.0f,
-  -0.25f, -0.25f, -0.75f, 1.0f,
-
-  -0.25f,  0.25f,  0.75f, 1.0f,
-  -0.25f, -0.25f, -0.75f, 1.0f,
-  -0.25f,  0.25f, -0.75f, 1.0f,
-
-  0.25f,  0.25f,  0.75f, 1.0f,
-  0.25f, -0.25f, -0.75f, 1.0f,
-  0.25f, -0.25f,  0.75f, 1.0f,
-
-  0.25f,  0.25f,  0.75f, 1.0f,
-  0.25f,  0.25f, -0.75f, 1.0f,
-  0.25f, -0.25f, -0.75f, 1.0f,
-
-  0.25f,  0.25f, -0.75f, 1.0f,
-  0.25f,  0.25f,  0.75f, 1.0f,
-  -0.25f,  0.25f,  0.75f, 1.0f,
-
-  0.25f,  0.25f, -0.75f, 1.0f,
-  -0.25f,  0.25f,  0.75f, 1.0f,
-  -0.25f,  0.25f, -0.75f, 1.0f,
-
-  0.25f, -0.25f, -0.75f, 1.0f,
-  -0.25f, -0.25f,  0.75f, 1.0f,
-  0.25f, -0.25f,  0.75f, 1.0f,
-
-  0.25f, -0.25f, -0.75f, 1.0f,
-  -0.25f, -0.25f, -0.75f, 1.0f,
-  -0.25f, -0.25f,  0.75f, 1.0f,
-};
+#include <vector>
+#include <cstdlib>
+#include <cassert>
 
 class loader
 {
+  std::vector<float> v;
 public:
-  const float* vertexData;
+  float* vertexData;
   int triangles;
   void load()
   {
-    vertexData = vertexData2;
-    triangles = sizeof(vertexData2) / 3;
+    const int width = 30;
+    const int height = 30;
+
+    float m[width][height];
+    for(int i = 0; i < width; ++i)
+      for(int j = 0; j < height; ++j)
+        m[i][j] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+    v.reserve(3 * 2 * (width - 1) * (height - 1));
+    for(int i = 0; i < width - 1; ++i)
+      for(int j = 0; j < height - 1; ++j)
+      {
+        v.push_back(m[i][j]);
+        v.push_back(m[i + 1][j]);
+        v.push_back(m[i][j + 1]);
+        
+        v.push_back(m[i + 1][j]);
+        v.push_back(m[i + 1][j + 1]);
+        v.push_back(m[i][j + 1]);
+      }
+
+    vertexData = &v[0];
+    triangles = v.size() / 3;
+    assert(v.size() == 3 * 2 * (width - 1) * (height - 1));
   }
 };
 
