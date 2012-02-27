7efd3bfde186e4daa545dcad052c31d962082209
foo <maciek.pacut@gmail.com>
2012-02-27 20:36:48 +0100

    Blit surface

--- python/lista1/zad2.py
+++ python/lista1/zad2.py
@@ -3,9 +3,12 @@ import math
 from pygame.locals import *
 from sys import exit
 
+screen_size = (640, 480)
+
 def init_game():
     global player_pos
     player_pos = (0,0)
+    prepare_background()
 
 def onmousemove(pos):
     global player_pos
@@ -14,8 +17,24 @@ def onmousemove(pos):
 def draw_circle(pos):
     pygame.draw.circle(screen, (255, 0, 0), pos, 10)
 
+def prepare_background():
+    global background
+    background = pygame.Surface(screen_size)
+    draw_gradient(background)
+
+def draw_background(screen):
+    global background
+    screen.blit(background, (0,0))
+
+def draw_gradient(screen):
+    for x in range(0, screen.get_width() - 1):
+        for y in range(0, screen.get_height() - 1):
+            color = (10, 0, 0)
+            screen.set_at((x, y), color)
+
 def draw():
-    print player_pos
+    global screen
+    draw_background(screen)
     draw_circle(player_pos)
 
 def frame():
@@ -27,7 +46,7 @@ def frame():
 def init_pygame():
     pygame.init()
     global screen
-    screen = pygame.display.set_mode( (640,480) )
+    screen = pygame.display.set_mode(screen_size)
 
 def loop():
     global screen
