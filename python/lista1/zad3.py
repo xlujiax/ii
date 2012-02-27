c8121f5d03c29e03c082f4d96d854709abb95c43
foo <maciek.pacut@gmail.com>
2012-02-27 21:25:26 +0100

    Grass with clouds

--- python/lista1/zad3.py
+++ python/lista1/zad3.py
@@ -7,7 +7,7 @@ from sys import exit
 size = (512, 512)
 
 bg = pygame.Surface(size)
-bg.fill((255,255,255))
+bg.fill((0,255,0))
 
 def cloud_level(level, color, points = 10, amplitude = 10):
     pts = []
@@ -25,10 +25,17 @@ def cloud_level(level, color, points = 10, amplitude = 10):
 
 
 levels = 10
-for i in range(1,levels):
+
+for i in range(0,4):
     y = size[1] - int(float(i) / float(levels) * float(size[1]))
-    brightness = float(i) / float(levels + 1) * 255
-    color = (int(brightness * 0.2), int(brightness * 0.2), int(brightness))
-    cloud_level(y, color)
+    brightness = float(4 + i) / float(8) * 255
+    color = (int(brightness * 0.2), int(brightness), int(brightness * 0.2))
+    cloud_level(y, color, 20)
+    
+for i in range(4,levels):
+    y = size[1] - int(float(i) / float(levels) * float(size[1]))
+    brightness = 255 - float(i) / float(levels + 1) * 128
+    color = (int(brightness * 0.1), int(brightness * 0.1), int(brightness))
+    cloud_level(y, color, 20)
 
 pygame.image.save(bg, "bg.jpg")
