1011270c676716b0b2fc7a7cb1042c67af4add10
foo <maciek.pacut@gmail.com>
2012-03-03 17:51:35 +0100

    Drawing palette

--- python/lista2/zad3.py
+++ python/lista2/zad3.py
@@ -6,18 +6,33 @@ class Palette:
         self.big_picture = pygame.image.load("PAVE.bmp")
         self.grid_size = 40
         self.partition()
+        print len(self.bricks)
 
     def partition(self):
         full_blocks = [(1, 1), (3, 1), (5, 1), (7, 1),
-                       (1, 3), (3, 3), (5, 3), (7, 3), (9, 3), (11, 3),
-                       (1, 5), (3, 5), (5, 5), (7, 5), (9, 5), (11, 5),
-                       (1, 7), (3, 7), (5, 7), (7, 7)]
-        self.bricks = [self.big_picture.subsurface(self.__rect(x,y))
+                       (1, 3), (3, 3), (5, 3), (7, 3), (9, 3),
+                       (1, 5), (3, 5), (5, 5), (7, 5), (9, 5),
+                       (1, 7), (3, 7), (5, 7)]
+        self.bricks = [self.big_picture.subsurface(self.__rect(x, y))
                        for (x, y) in full_blocks]
 
     def __rect(self, x, y):
         return (x * self.grid_size, y * self.grid_size,
                 self.grid_size, self.grid_size)
+
+    def draw(self, surface, pos, rows):
+        margin = 3
+        x = 0
+        y = 0
+        for brick in self.bricks:
+            brick_pos = (pos[0] + x * (self.grid_size + margin),
+                         pos[1] + y * (self.grid_size + margin))
+            surface.blit(brick, brick_pos)
+            x = x + 1
+            if x == rows:
+                y = y + 1
+                x = 0
+            
     
 class Editor:
     def __init__(self):
@@ -29,7 +44,8 @@ class Editor:
     def frame(self):
         "Frame of animation"
         self.screen.fill((255, 255, 255))
-        self.screen.blit(self.palette.bricks[0], (0, 0))
+
+        self.palette.draw(self.screen, (10, 10), 2)
         pygame.display.update()
 
     def onmousemove(self, pos):
