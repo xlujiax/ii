b0d35190e097ecab4868a24f335afa14130113b3
foo <maciek.pacut@gmail.com>
2012-03-03 17:39:20 +0100

    Palette

--- python/lista2/zad3.py
+++ python/lista2/zad3.py
@@ -1,19 +1,35 @@
 "Maciej Pacut, Z3, L2"
 import pygame
 
-class Editor:
-    "Edytor Jednej Kreski"
+class Palette:
+    def __init__(self):
+        self.big_picture = pygame.image.load("PAVE.bmp")
+        self.grid_size = 40
+        self.partition()
 
+    def partition(self):
+        full_blocks = [(1, 1), (3, 1), (5, 1), (7, 1),
+                       (1, 3), (3, 3), (5, 3), (7, 3), (9, 3), (11, 3),
+                       (1, 5), (3, 5), (5, 5), (7, 5), (9, 5), (11, 5),
+                       (1, 7), (3, 7), (5, 7), (7, 7)]
+        self.bricks = [self.big_picture.subsurface(self.__rect(x,y))
+                       for (x, y) in full_blocks]
+
+    def __rect(self, x, y):
+        return (x * self.grid_size, y * self.grid_size,
+                self.grid_size, self.grid_size)
+    
+class Editor:
     def __init__(self):
         pygame.init()
-        self.palette = pygame.image.load("PAVE.bmp")
+        self.palette = Palette()
         self.screen_size = (640, 480)
         self.screen = pygame.display.set_mode(self.screen_size)
 
     def frame(self):
         "Frame of animation"
-        self.screen.blit(self.palette, (0, 0))
-
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.palette.bricks[0], (0, 0))
         pygame.display.update()
 
     def onmousemove(self, pos):
