20d6dcffadd102d6a2dc0ea019a41ba3af5d091c
foo <maciek.pacut@gmail.com>
2012-03-03 19:23:01 +0100

    Save and load work

--- python/lista2/zad3.py
+++ python/lista2/zad3.py
@@ -1,21 +1,44 @@
 "Maciej Pacut, Z3, L2"
 import pygame
+import os
 
 class Brick:
-    def __init__(self, img, pos):
+    def __init__(self, img_index, pos, palette):
         self.pos = pos
-        self.img = img
+        self.img_index = img_index
+        self.palette = palette
+
+    def serialize(self):
+        return (self.pos, self.img_index)
 
     def draw(self, surface):
-        surface.blit(self.img, self.pos)
+        surface.blit(self.palette.get_img(self.img_index), self.pos)
+
+def unserialize(brick_data, palette):
+    return Brick(brick_data[1], brick_data[0], palette)
 
 class Playground:
-    def __init__(self, pos, grid_size, rows, cols):
+    def __init__(self, pos, grid_size, rows, cols, palette):
         self.pos = pos
         self.grid_size = grid_size
         self.rect = (pos[0], pos[1], grid_size * cols,
                      grid_size * rows)
         self.bricks = []
+        self.palette = palette
+
+    def load_bricks(self, filename):
+        "Load from file"
+        if os.path.isfile(filename):
+            line_file = open(filename, 'r')
+            raw_data = eval(line_file.read())
+            self.bricks = [unserialize(brick, self.palette) for brick in raw_data] # sploit alert
+        else:
+            self.bricks = []
+
+    def save_bricks(self, filename):
+        "Save to file"
+        line_file = open(filename, 'w')
+        line_file.write(str([brick.serialize() for brick in self.bricks]))
         
     def draw(self, surface):
         pygame.draw.rect(surface, (0, 0, 0), self.rect, 3)
@@ -30,11 +53,12 @@ class Playground:
         self.bricks.append(brick)
 
 class Grab:
-    def __init__(self, mouse_pos, brick, playground):
+    def __init__(self, mouse_pos, brick, playground, palette):
         self.brick = brick
         self.delta = (brick.pos[0] - mouse_pos[0],
                       brick.pos[1] - mouse_pos[1])
         self.playground = playground
+        self.palette = palette
 
     def draw(self, surface, mouse_pos, playground):
         "Takes actual mouse_pos"
@@ -47,9 +71,9 @@ class Grab:
     def sticky_brick(self):
         xindex = (self.brick.pos[0] - self.delta[0] - self.playground.pos[0]) / self.playground.grid_size
         yindex = (self.brick.pos[1] - self.delta[1] - self.playground.pos[1]) / self.playground.grid_size
-        return Brick(self.brick.img,
+        return Brick(self.brick.img_index,
                      (self.playground.pos[0] + xindex * self.playground.grid_size,
-                      self.playground.pos[1] + yindex * self.playground.grid_size))
+                      self.playground.pos[1] + yindex * self.playground.grid_size), self.palette)
 
     def draw_shadow(self, surface, mouse_pos):
         "Sticky shadow" 
@@ -96,25 +120,32 @@ class Palette:
     def grab_brick(self, mouse_pos):
         x = 0
         y = 0
+        index = 0
         for brick_surface in self.bricks:
             brick_pos = (self.pos[0] + x * (self.grid_size + self.margin),
                          self.pos[1] + y * (self.grid_size + self.margin))
             collision_rect = (brick_pos[0], brick_pos[1],
                               self.grid_size, self.grid_size)
             if pygame.Rect(collision_rect).collidepoint(mouse_pos):
-                return Brick(brick_surface, brick_pos)
+                return Brick(index, brick_pos, self)
             x = x + 1
             if x == self.rows:
                 y = y + 1
                 x = 0
+            index = index + 1
         return None
+
+    def get_img(self, img_index):
+        return self.bricks[img_index]
+
 
 class Editor:
     def __init__(self):
         pygame.init()
         self.palette = Palette((10, 10), 2)
 
-        self.playground = Playground((100, 10), 40, 10, 10)
+        self.playground = Playground((100, 10), 40, 10, 10, self.palette)
+        self.playground.load_bricks("playground.txt")
 
         self.screen_size = (640, 480)
         self.screen = pygame.display.set_mode(self.screen_size)
@@ -141,7 +172,7 @@ class Editor:
         "MOUSEBUTTONDOWN event"
         brick = self.palette.grab_brick(pos)
         if brick != None:
-            self.grab = Grab(pos, brick, self.playground)
+            self.grab = Grab(pos, brick, self.playground, self.palette)
 
     def onmouseup(self, pos):
         "MOUSEBUTTONUP event"
@@ -149,11 +180,15 @@ class Editor:
             self.playground.add_brick(self.grab.sticky_brick())
             self.grab = None
 
+    def onexit(self):
+        self.playground.save_bricks("playground.txt")
+
     def loop(self):
         "Main loop"
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    self.onexit()
                     pygame.quit()
                     exit(0)
                 elif event.type == pygame.MOUSEMOTION:
