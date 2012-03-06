fbcf80851ac594e3acc916dc7c3ceea6f9f8d09e
Maciej Pacut <i221082@nevelson.stud.ii>
2012-03-06 13:30:21 +0100

    3x3

--- python/lista3/zad1.py
+++ python/lista3/zad1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pygame
+import os
 from pygame.locals import *
 from sys import exit
 from random import randint
@@ -9,6 +10,18 @@ def draw(pos, color):
   (xm, ym) = pos
   x = xm / 10
   y = ym / 10
+  draw_aux(x, y, color)
+  if a3x3:
+    draw_aux(x - 1, y - 1, color)
+    draw_aux(x - 1, y, color)
+    draw_aux(x - 1, y + 1, color)
+    draw_aux(x, y - 1, color)
+    draw_aux(x, y + 1, color)
+    draw_aux(x + 1, y - 1, color)
+    draw_aux(x + 1, y, color)
+    draw_aux(x + 1, y + 1, color)
+
+def draw_aux(x, y, color):
   if x >= 0 and x < 64 and y >= 0 and y < 48:
     image.set_at((x, y), color)
 
@@ -32,10 +45,13 @@ fillColor = [(0, 0, 0),
 colorIndex = 0
 
 small = False
+a3x3 = False
+mousePos = (0, 0)
 
 while True:
   for event in pygame.event.get():
     if event.type == QUIT:
+      pygame.image.save(image, "rys.bmp")
       exit()
     if event.type == MOUSEBUTTONDOWN:
       if event.button == 3:
@@ -47,6 +63,7 @@ while True:
       mouseButtonDown = True
 
     if event.type == MOUSEMOTION:
+      mousePos = event.pos
       if mouseButtonDown:
         if rightButton:
           draw(event.pos, clearColor)
@@ -68,13 +85,22 @@ while True:
     if key[pygame.K_SPACE]: small = True
     else: small = False
     
+    if key[pygame.K_LSHIFT]: a3x3 = True
+    else: a3x3 = False
+    
     if small:
       screen.fill(clearColor)
       screen.blit(image, (0, 0))
     else:
       working = pygame.Surface((640, 480))
+      working.convert(image)
       pygame.transform.scale(image, (640, 480), working)
       screen.blit(working, (0, 0))
+
+      if a3x3:
+        cursor = pygame.Surface((30,30))
+        cursor.fill((128, 128, 128))
+        screen.blit(cursor, (((mousePos[0] / 10) - 1) * 10, ((mousePos[1] / 10) - 1) * 10))
      
     pygame.display.update()
     
